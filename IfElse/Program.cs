fe13e4925c9e47028fa995b4ab633afff8e1e940
MihaLis333 <m190333@ya.ru>
2023-02-08 23:21:18 +0300

    test ifElse and comand tolower

---
+++ IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя: ");
+string name = Console.ReadLine();
+
+if(name.ToLower == "Маша")
+{
+    Console.WriteLine("Ура, это же Маша");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(name);
+}
