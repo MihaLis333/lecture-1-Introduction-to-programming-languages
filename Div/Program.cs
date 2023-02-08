e38e4532097bdf2df1455cbb05f8664dc2e84bee
MihaLis333 <m190333@ya.ru>
2023-02-08 22:48:46 +0300

    test comand random

--- Div/Program.cs
+++ Div/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int numA = new Random().Next(1,10);
+Console.WriteLine(numA);
+int numB = new Random().Next(1,10);
+Console.WriteLine(numB);
+Console.WriteLine(numA + numB);
